3a5b4fb8f1339d5ac582f5836ded6308ffd4b3f1
jkwon-dev <kwonejk@gmail.com>
2023-12-11 15:46:48 +0900

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by kwon eunji on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
